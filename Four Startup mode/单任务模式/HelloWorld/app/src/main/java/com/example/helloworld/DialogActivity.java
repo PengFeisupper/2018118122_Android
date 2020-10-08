55f19e5da770a27da6ded7eeb2e012a7100f1c77
LAPTOP-M2PV68FF\KP <1934280654@qq.com>
2020-10-08 13:06:00 +0800

    单任务模式

---
+++ Four Startup mode/单任务模式/HelloWorld/app/src/main/java/com/example/helloworld/DialogActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("DialogActivity",this.toString());
+        setContentView(R.layout.dialog_layout);
+        Button startMainActivity = (Button) findViewById(R.id.start_main_activity);
+
+        startMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DialogActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    protected  void onDestroy() {
+        super.onDestroy();
+        Log.d("DialogActivity","onDestroy");
+    }
+}
---
+++ Four Startup mode/单任务模式/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public static final  String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,this.toString());
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+   /* protected  void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    protected  void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    protected  void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    protected  void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    protected  void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","onDestroy");
+    }*/
+    protected  void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity","onRestart");
+    }
+
+
+
+}
